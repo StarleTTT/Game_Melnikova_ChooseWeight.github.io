cf9edf8dc563e8a501c38a241925631158f2142d
Elena Melnikova <90891282+StarleTTT@users.noreply.github.com>
2024-02-12 18:29:52 +0300

    Update рейтинг.js

--- рейтинг.js
+++ рейтинг.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
     var storedData = localStorage.getItem('users');
-    var users = JSON.parse(storedData)
-users.forEach(function(users){
-    let row = document.createElement('tr')
-    row.innerHTML = `<td >${users.username}</td><td>${users.count}</td>`
-    document.querySelector('.leaderboard').appendChild(row)
+    var users = JSON.parse(storedData);
+
+    // Сортировка пользователей по убыванию значения count
+    users.sort((a, b) => b.count - a.count);
+
+    users.forEach(function(user){
+        let row = document.createElement('tr');
+        row.innerHTML = `<td>${user.username}</td><td>${user.count}</td>`;
+        document.querySelector('.leaderboard').appendChild(row);
+    });
 });
+
+function alerted(){
+    window.location.href = "index.html";
 }
-    )
-   function alerted(){
-        window.location.href = "авторизация.html";}
